bbf67cd6d1309da37e53ce4ce07d3d63cab4ceaf
nyoung <kembel0116@gmail.com>
2025-03-27 10:24:48 +0900

    .

--- models/csae.py
+++ models/csae.py
@@ -12,22 +12,24 @@ class CSAE(nn.Module):
       latent_dim = 256,
       topk = 50,
       num_classes = 100,
-      num_parallel_layers = 5):
-    super.__init__()
+      num_parallel_layers = 4):
+    super().__init__()
     self.backbone = ViTBackbone(model_name=vit_model)
     self.hidden_dim = self.backbone.hidden_dim 
     self.sae = SparseAutoencoder(input_dim=self.hidden_dim, latent_dim=latent_dim, topk=topk)
     self.num_parallel_layers = num_parallel_layers  
     self.classifier = ClassifierHead(input_dim=self.hidden_dim, num_classes=num_classes)
 
-  def forward(self, images):
+  def forward(self, images, prev_weight = None):
     x = self.backbone(images)
     latent_list = []
+
+    current_weight = prev_weight
 
     for i in range(self.num_parallel_layers):
       layer = self.backbone.vit.encoder.layer[i]
       wrapper = SaeWrapper(layer, self.sae)
-      x, latent = wrapper(x)
+      x, latent, current_weight = wrapper(x, prev_weight = current_weight)
       latent_list.append(latent)
 
     for i in range(self.num_parallel_layers, len(self.backbone.vit.encoder.layer)):
@@ -36,4 +38,4 @@ class CSAE(nn.Module):
     x = self.backbone.vit.layernorm(x)
     x = self.backbone.vit.pooler(x)
     logits = self.classifier(x)
-    return logits, latent_list
+    return logits, latent_list, current_weight
