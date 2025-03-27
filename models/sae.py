f66014cf8759b5624ddddb69f57e250e08a87043
nyoung <kembel0116@gmail.com>
2025-03-27 20:07:58 +0900

    .

--- models/sae.py
+++ models/sae.py
@@ -9,22 +9,29 @@ class SparseAutoencoder(nn.Module):
     self.topk = topk
     self.encoder = nn.Linear(input_dim, latent_dim)
     self.decoder = nn.Linear(latent_dim, input_dim)
+    self.alpha = nn.Parameter(torch.tensor(1.0))
 
-  def forward(self, x):
+  def forward(self, x, prev_weight = None):
     B, L, D = x.size()
     x_flat = x.view(B*L, D)
+
     latent = self.encoder(x_flat)
-    latent = torch.relu(latent)
 
     if self.topk is not None and self.topk < self.latent_dim:
       topk_vals, topk_idx = torch.topk(latent.abs(), k=self.topk, dim = 1)
       mask = torch.zeros_like(latent)
       mask.scatter_(1, topk_idx, 1)
-      gated_latent = latent *mask
+      gated_latent = latent * mask
     else:
       gated_latent = latent
-
-    mod_flat = self.decoder(gated_latent)
+    if prev_weight is not None:
+      prev_weight.requires_grad = False
+      delta = self.decoder.weight - prev_weight
+      W = prev_weight + self.alpha * delta
+    else:
+      W = self.decoder.weight
+    
+    mod_flat = gated_latent @ W.T
     mod = mod_flat.view(B, L, D)
 
-    return mod, latent
+    return mod, latent, W
