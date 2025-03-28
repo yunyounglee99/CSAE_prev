754086a6dd9d4c895ddb61ffd8cb06bd802b2215
nyoung <kembel0116@gmail.com>
2025-03-28 18:42:14 +0900

    .

--- train/train.py
+++ train/train.py
@@ -1,3 +1,10 @@
+# train/train.py
+import sys
+import os
+
+# 현재 파일(train.py)의 상위 디렉터리를 sys.path에 추가
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import torch
 import torch.nn.functional as F
 from torch import optim
@@ -5,9 +12,10 @@ from models.backbone import ViTBackbone
 from models.sae import SparseAutoencoder
 from models.classifier import ClassifierHead
 from models.csae import CSAE
-from dataloader import dataloaders
+from dload import dataloaders
+from tqdm import tqdm
 
-def train(
+def train_csae(
     num_tasks = 10,
     batch_size = 128,
     epochs = 20,
@@ -24,6 +32,7 @@ def train(
   else:
     divice_name = "cpu"
   device = torch.device(device_name)
+  print(device)
 
   model = CSAE(vit_model=vit_model, latent_dim=latent_dim, topk=topk, num_classes=100, num_parallel_layers=5)
   model.to(device)
@@ -32,17 +41,19 @@ def train(
   optimizer = optim.Adam(list(model.sae.parameters()) + list(model.classifier.parameters()), lr = lr)
   train_loaders, test_loaders = dataloaders(batch_size=batch_size, num_tasks=num_tasks)
 
+  prev_weight = None
+  
   for task_id, (train_loader, test_loader) in enumerate(zip(train_loaders, test_loaders)):
     print(f"\n=================Task {task_id}==================================")
     model.sae.train()
     model.classifier.train()
     total_loss = 0.0
 
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs)):
       for images, labels in train_loader:
         images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
-        logits, latent_list = model(images)
+        logits, latent_list, current_weight = model(images, prev_weight = prev_weight)
         ce_loss = F.cross_entropy(logits, labels)
         l1_loss = torch.mean(torch.abs(latent_list[-1]))
         loss = ce_loss + l1_loss
@@ -52,8 +63,11 @@ def train(
       print(f'Task {task_id} Epoch {epoch+1}/{epochs} loss : {total_loss/len(train_loader):.4f}')
       total_loss = 0.0
 
+    prev_weight = current_weight
+
     checkpoint = {"csae" : model.state_dict()}
     torch.save(checkpoint, f"checkpoint_task{task_id}.pth")
+    print(f"task {task_id} is saved : checkpoint_task{task_id}.pth")
 
     model.sae.eval()
     model.classifier.eval()
@@ -61,7 +75,7 @@ def train(
     with torch.no_grad():
       for images, labels in test_loader:
         images, labels = images.to(device), labels.to(device)
-        logits, _ = model(images)
+        logits, _, _ = model(images)
         preds = logits.argmax(dim=1)
         correct += (preds == labels).sum().item()
         total += labels.size(0)
@@ -69,4 +83,4 @@ def train(
     print(f"Task {task_id} acc : {acc:.2f}%")
 
 if __name__ == "__main__":
-  train()
+  train_csae()
