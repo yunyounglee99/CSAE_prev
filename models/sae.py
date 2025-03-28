34f05c54a31b71b2f497d2c6b187a611c27aab13
nyoung <kembel0116@gmail.com>
2025-03-28 18:41:01 +0900

    .

--- models/sae.py
+++ models/sae.py
@@ -9,12 +9,20 @@ class SparseAutoencoder(nn.Module):
     self.topk = topk
     self.encoder = nn.Linear(input_dim, latent_dim)
     self.decoder = nn.Linear(latent_dim, input_dim)
-    self.alpha = nn.Parameter(torch.tensor(1.0))
+    self.s = nn.Parameter(torch.tensor(0.0))
 
-  def forward(self, x, prev_weight = None):
+  def get_alpha(self, task_id):
+    if task_id == 0:
+      return torch.tensor(1.0, device = self.s.device)
+    else:
+      lower_bound = 0.0
+      upper_bound = 1.0 / (task_id + 1)
+      alpha = lower_bound + upper_bound * torch.sigmoid(self.s)
+      return alpha
+    
+  def forward(self, x, prev_weight = None, task_id = 0):
     B, L, D = x.size()
     x_flat = x.view(B*L, D)
-
     latent = self.encoder(x_flat)
 
     if self.topk is not None and self.topk < self.latent_dim:
@@ -24,10 +32,12 @@ class SparseAutoencoder(nn.Module):
       gated_latent = latent * mask
     else:
       gated_latent = latent
+
     if prev_weight is not None:
-      prev_weight.requires_grad = False
+      prev_weight = prev_weight.detach()
       delta = self.decoder.weight - prev_weight
-      W = prev_weight + self.alpha * delta
+      alpha = self.get_alpha(task_id)
+      W = prev_weight + alpha * delta
     else:
       W = self.decoder.weight
     
