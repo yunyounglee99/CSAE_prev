579a33342a23dbd8f130f67f245a0d416de78b9f
Nyoungs <136971905+yunyounglee99@users.noreply.github.com>
2025-04-06 21:51:45 +0900

    Delete models directory

--- models/classifier.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-class ClassifierHead(nn.Module):
-  def __init__(self, input_dim = 768, num_classes = 100):
-    super().__init__()
-    self.classifier = nn.Linear(input_dim, num_classes)
-
-  def forward(self, x):
-    return self.classifier(x)
